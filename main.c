7c5c0ae32d1de82bf37c93925a68a18407425d0e
Madjid865 <madjidchikhi21@gmail.com>
2024-12-08 22:12:13 +0100

    Version 4 : texture de la fusee et gestion des evenements du clavier finis, passer aux asteroides

--- main.c
+++ main.c
@@ -1,24 +1,20 @@
-// gcc main.c -o prog $(sdl2-config --cflags --libs)
-// ./prog
-// Token : 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
-
 #include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-//Fenetre
+// Fenetre
 #define LARGEUR_FENETRE 1024
 #define HAUTEUR_FENETRE 800
 
-//Boutons
-#define LARGEUR_BOUTON 300 //Correspond à la la largeur de l'image du bouton 
-#define HAUTEUR_BOUTON 100 //Correspond à la la hauteur de l'image du bouton 
+// Boutons
+#define LARGEUR_BOUTON 300
+#define HAUTEUR_BOUTON 100
 
-//Fusee
-#define LARGEUR_FUSEE 100 //Correspond à la la largeur de l'image du bouton 
-#define HAUTEUR_FUSEE 100 //Correspond à la la hauteur de l'image du bouton 
+// Fusee
+#define LARGEUR_FUSEE 100
+#define HAUTEUR_FUSEE 100
 
-//Gestion des erreurs avant creation fenetre et rendu
+// Gestion des erreurs avant creation fenetre et rendu
 void SDL_ExitWithError(const char *message)
 {
     SDL_Log("ERREUR : %s > %s\n", message, SDL_GetError());
@@ -26,214 +22,207 @@ void SDL_ExitWithError(const char *message)
     exit(EXIT_FAILURE);
 }
 
-//Gestion des erreurs et destruction fenetre et rendu
-void SDL_ExitWithErrorAndDeleteWR(SDL_Window *window, SDL_Renderer *renderer,const char *message)
+// Gestion des erreurs et destruction fenetre et rendu
+void SDL_ExitWithErrorAndDeleteWR(SDL_Window *window, SDL_Renderer *renderer, const char *message)
 {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_ExitWithError(message);
 }
 
-//Creation textures arrire plan & boutons start et exit
-void MySDL_CreateTexture(SDL_Window *window, SDL_Renderer *renderer, SDL_Surface **image_bouton, SDL_Texture **texture_bouton ,const char *chemin_image_bouton)
+// Creation textures arriere-plan & boutons start et exit
+void MySDL_CreateTexture(SDL_Window *window, SDL_Renderer *renderer, SDL_Surface **image, SDL_Texture **texture, const char *chemin_image)
 {
-    //Chargement image bouton
-    *image_bouton = SDL_LoadBMP(chemin_image_bouton);
-    if(*image_bouton == NULL)
-        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement image bouton");
-        
-    //Creation texture bouton
-    *texture_bouton = SDL_CreateTextureFromSurface(renderer, *image_bouton);
-    SDL_FreeSurface(*image_bouton);
-    if(*texture_bouton == NULL)
-        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Creation texture bouton");
+    // Chargement image
+    *image = SDL_LoadBMP(chemin_image);
+    if (*image == NULL)
+        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement image");
+
+    // Creation texture
+    *texture = SDL_CreateTextureFromSurface(renderer, *image);
+    SDL_FreeSurface(*image);
+    if (*texture == NULL)
+        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Creation texture");
 }
 
-//Creation fusee
-void creationFusee(SDL_Window *window, SDL_Renderer *renderer, SDL_Texture **texture_bouton_start, SDL_Texture **texture_bouton_exit)
+int main(int argc, char *argv[])
 {
+    // Initialisation
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
+    // Lancement SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        SDL_ExitWithError("Initialisation SDL");
+
+    // Creation fenetre + rendu
+    if (SDL_CreateWindowAndRenderer(LARGEUR_FENETRE, HAUTEUR_FENETRE, 0, &window, &renderer) != 0)
+        SDL_ExitWithError("Creation fenetre et rendu");
+
+    // Execution du programme
+
+    /*-----------Arriere plan------------*/
+
+    SDL_Surface *image_arriere_plan = NULL;
+    SDL_Texture *texture_arriere_plan = NULL;
+    
+    //Creation texture arriere plan
+    MySDL_CreateTexture(window, renderer, &image_arriere_plan, &texture_arriere_plan, "/home/madjid/Documents/C/Projet/Images/subtle_space_background.bmp");
+        
+    //Encadrement pour la texture arriere plan
+    SDL_Rect rectangle_texture;
+    
+    //Chargement en memoire de la texture arriere plan
+    if(SDL_QueryTexture(texture_arriere_plan, NULL, NULL, &rectangle_texture.w, &rectangle_texture.h) != 0)
+        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement en memoire texture arriere plan");
+        
+    /*Rajouter ces 2 ligne pour que la texture occupe toute la fenetre
+    rectangle_texture.w = LARGEUR_FENETRE;
+    rectangle_texture.h = HAUTEUR_FENETRE;
+    */
+	
+    //Centrer la texture par rapport a la fenetre
+    rectangle_texture.x = (LARGEUR_FENETRE - rectangle_texture.w) / 2;
+    rectangle_texture.y = (HAUTEUR_FENETRE - rectangle_texture.h) / 2;
+
+    /*-----------Boutons------------*/
+  
     //Initialisation
+    SDL_Surface *image_bouton_start = NULL;
+    SDL_Surface *image_bouton_exit = NULL;
+    SDL_Texture *texture_bouton_start = NULL;
+    SDL_Texture *texture_bouton_exit = NULL;
+
+    //Encadrement pour bouton start
+    SDL_Rect rectangle_bouton_start = {LARGEUR_FENETRE / 2 - LARGEUR_BOUTON / 2, HAUTEUR_FENETRE / 3 - HAUTEUR_BOUTON / 2, 0, 0};
+    
+    //Creation texture bouton start
+    MySDL_CreateTexture(window, renderer, &image_bouton_start, &texture_bouton_start, "/home/madjid/Documents/C/Projet/Images/start.bmp");
+
+    //Chargement en memoire de la texture bouton start
+    if (SDL_QueryTexture(texture_bouton_start, NULL, NULL, &rectangle_bouton_start.w, &rectangle_bouton_start.h) != 0)
+        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement texture bouton start");
+
+    //Encadrement pour bouton exit
+    SDL_Rect rectangle_bouton_exit = {LARGEUR_FENETRE / 2 - LARGEUR_BOUTON / 2, 2 * HAUTEUR_FENETRE / 3 - HAUTEUR_BOUTON / 2, 0, 0};
+    
+    //Creation texture bouton exit
+    MySDL_CreateTexture(window, renderer, &image_bouton_exit, &texture_bouton_exit, "/home/madjid/Documents/C/Projet/Images/exit.bmp");
+
+    //Chargement en memoire de la texture bouton exit
+    if (SDL_QueryTexture(texture_bouton_exit, NULL, NULL, &rectangle_bouton_exit.w, &rectangle_bouton_exit.h) != 0)
+        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement texture bouton exit");
+
+    /*----------------Fusee-----------------*/
+
+    // Initialisation
     SDL_Surface *image_fusee = NULL;
     SDL_Texture *texture_fusee = NULL;
-
+    
     //Encadrement pour la fusee
-    SDL_Rect rectangle_fusee = {LARGEUR_FENETRE/2 - LARGEUR_FUSEE/2, HAUTEUR_FENETRE/2 - HAUTEUR_FUSEE/2, LARGEUR_FUSEE, HAUTEUR_FUSEE};
-
+    SDL_Rect rectangle_fusee = {LARGEUR_FENETRE / 2 - LARGEUR_FUSEE / 2, HAUTEUR_FENETRE - 2 * HAUTEUR_FUSEE, 0, 0};
+    
     //Creation texture fusee
     MySDL_CreateTexture(window, renderer, &image_fusee, &texture_fusee, "/home/madjid/Documents/C/Projet/Images/fusee.bmp");
 
     //Chargement en memoire de la texture fusee
-    if(SDL_QueryTexture(texture_fusee, NULL, NULL, &rectangle_fusee.w, &rectangle_fusee.h) != 0)
-        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement en memoire texture fusee");
-        
-    //Affichage image fusee
-    if(SDL_RenderCopy(renderer, texture_fusee, NULL, &rectangle_fusee) != 0)
-        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Affichage texture fusee");
-        
-    SDL_DestroyTexture(*texture_bouton_start);
-    SDL_DestroyTexture(*texture_bouton_exit);
+    if (SDL_QueryTexture(texture_fusee, NULL, NULL, &rectangle_fusee.w, &rectangle_fusee.h) != 0)
+        SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement texture fusee");
+
+    // Afficher ou pas les boutons (affcihes par defaut)
+    SDL_bool afficher_boutons = SDL_TRUE;
+
+    //MAJ du rendu 
     SDL_RenderPresent(renderer);
-}
 
+    /*----------------------------------------------------------------------------------*/
+    // Gestion des evenements
 
-int main (int argc, char *argv[])
-{
-        //Initialisation
-        SDL_Window *window = NULL;
-        SDL_Renderer *renderer = NULL;
+    SDL_bool program_launched = SDL_TRUE;
+    while (program_launched)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_RIGHT:
+                    rectangle_fusee.x += 10;
+                    continue;
+                case SDLK_LEFT:
+                    rectangle_fusee.x -= 10;
+                    continue;
+                case SDLK_UP:
+                    rectangle_fusee.y -= 7.8125;
+                    continue;
+                case SDLK_DOWN:
+                    rectangle_fusee.y += 7.8125;
+                    continue;
+                default:
+                    continue;
+                }
+
+            case SDL_MOUSEBUTTONDOWN:
+                if (event.button.button == SDL_BUTTON_LEFT)
+                {
+                    int x = event.button.x;
+                    int y = event.button.y;
+
+                    if (x >= rectangle_bouton_start.x && x <= rectangle_bouton_start.x + rectangle_bouton_start.w &&
+                        y >= rectangle_bouton_start.y && y <= rectangle_bouton_start.y + rectangle_bouton_start.h)
+                    {
+                        afficher_boutons = SDL_FALSE;
+                    }
+
+                    if (x >= rectangle_bouton_exit.x && x <= rectangle_bouton_exit.x + rectangle_bouton_exit.w &&
+                        y >= rectangle_bouton_exit.y && y <= rectangle_bouton_exit.y + rectangle_bouton_exit.h)
+                    {
+                        program_launched = SDL_FALSE;
+                    }
+                }
+                break;
+
+            case SDL_QUIT:
+                program_launched = SDL_FALSE;
+                break;
+
+            default:
+                break;
+            }
+        }
         
-        //Lancement SDL
-	if(SDL_Init(SDL_INIT_VIDEO) != 0)
-	    SDL_ExitWithError("Initialisation SDL");
-	
-	//Creation fenetre + rendu
-	if(SDL_CreateWindowAndRenderer(LARGEUR_FENETRE, HAUTEUR_FENETRE, 0, &window, &renderer) != 0)
-	    SDL_ExitWithError("Creation fenetre et rendu");
-	
-	//Execution du programme
-	
-	/*-----------Arriere plan------------*/ 
-	
-	//Initialisation
-	SDL_Surface *image_arriere_plan = NULL;
-        SDL_Texture *texture_arriere_plan = NULL;
-        
-        //Creation texture arriere plan
-        MySDL_CreateTexture(window, renderer, &image_arriere_plan, &texture_arriere_plan, "/home/madjid/Documents/C/Projet/Images/subtle_space_background.bmp");
-        
-        //Encadrement pour la texture arriere plan
-	SDL_Rect rectangle_texture;
-	
-        //Chargement en memoire de la texture arriere plan
-        if(SDL_QueryTexture(texture_arriere_plan, NULL, NULL, &rectangle_texture.w, &rectangle_texture.h) != 0)
-            SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement en memoire texture arriere plan");
-        
-	/*Rajouter ces 2 ligne pour que la texture occupe toute la fenetre
-	rectangle_texture.w = LARGEUR_FENETRE;
-	rectangle_texture.h = HAUTEUR_FENETRE;
-	*/
-	
-	//Centrer la texture par rapport a la fenetre
-	rectangle_texture.x = (LARGEUR_FENETRE - rectangle_texture.w) / 2;
-	rectangle_texture.y = (HAUTEUR_FENETRE - rectangle_texture.h) / 2;
-	
-        //Affichage image arriere plan
-        if(SDL_RenderCopy(renderer, texture_arriere_plan, NULL, &rectangle_texture) != 0)
-            SDL_ExitWithErrorAndDeleteWR(window, renderer, "Affichage texture arriere plan");
-         
-        /*-----------Boutons------------*/  
-        
-        //Initialisation
-        SDL_Surface *image_bouton_start = NULL;
-        SDL_Surface *image_bouton_exit = NULL;
-        SDL_Texture *texture_bouton_start = NULL;
-        SDL_Texture *texture_bouton_exit = NULL;
-        
-        //Encadrement pour bouton start
-	SDL_Rect rectangle_bouton_start = {LARGEUR_FENETRE/2 - LARGEUR_BOUTON/2, HAUTEUR_FENETRE/3 - HAUTEUR_BOUTON/2, LARGEUR_BOUTON, HAUTEUR_BOUTON};
-        
-        //Creation texture bouton start
-        MySDL_CreateTexture(window, renderer, &image_bouton_start, &texture_bouton_start, "/home/madjid/Documents/C/Projet/Images/start.bmp");
-	
-	//Chargement en memoire de la texture bouton start
-        if(SDL_QueryTexture(texture_bouton_start, NULL, NULL, &rectangle_bouton_start.w, &rectangle_bouton_start.h) != 0)
-            SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement en memoire texture bouton start");
-            
-        //Affichage image bouton start
-        if(SDL_RenderCopy(renderer, texture_bouton_start, NULL, &rectangle_bouton_start) != 0)
-            SDL_ExitWithErrorAndDeleteWR(window, renderer, "Affichage texture bouton start");
-         
-        //Creation texture bouton exit
-        MySDL_CreateTexture(window, renderer, &image_bouton_exit, &texture_bouton_exit, "/home/madjid/Documents/C/Projet/Images/exit.bmp");
-        
-        //Encadrement pour bouton exit
-	SDL_Rect rectangle_bouton_exit = {LARGEUR_FENETRE/2 - LARGEUR_BOUTON/2, 2*HAUTEUR_FENETRE/3 - HAUTEUR_BOUTON/2, LARGEUR_BOUTON, HAUTEUR_BOUTON};
-            
-        //Chargement en memoire de la texture bouton exit
-        if(SDL_QueryTexture(texture_bouton_exit, NULL, NULL, &rectangle_bouton_exit.w, &rectangle_bouton_exit.h) != 0)
-            SDL_ExitWithErrorAndDeleteWR(window, renderer, "Chargement en memoire texture bouton exit");
-            
-        //Affichage image bouton exit
-        if(SDL_RenderCopy(renderer, texture_bouton_exit, NULL, &rectangle_bouton_exit) != 0)
-            SDL_ExitWithErrorAndDeleteWR(window, renderer, "Affichage texture bouton exit");
-        
-        //MAJ du rendu   
+        // Effacer les textures qui sont sur le rendu et afficher seuelement l'arriere plan
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture_arriere_plan, NULL, &rectangle_texture);
+
+        // Afficher ou les boutons ou la fusee en plus de l'arriere plan
+        if (afficher_boutons)
+        {
+            SDL_RenderCopy(renderer, texture_bouton_start, NULL, &rectangle_bouton_start);
+            SDL_RenderCopy(renderer, texture_bouton_exit, NULL, &rectangle_bouton_exit);
+        }
+        else
+        {
+            SDL_RenderCopy(renderer, texture_fusee, NULL, &rectangle_fusee);
+        }
+
+        // MAJ rendu
         SDL_RenderPresent(renderer);
-	
-	/*----------------------------------------------------------------------------------*/
-	//Gestion des evenements
+    }
 
-	SDL_bool program_launched = SDL_TRUE;
-	 while(program_launched)
-	 {
-	    SDL_Event event;
-	    while(SDL_PollEvent(&event))
-	    {
-	        switch(event.type)
-	        {
-	            //Evenements clavier
-	            case SDL_KEYDOWN:
-	                switch(event.key.keysym.sym)
-	                {
-	                    case SDLK_RIGHT:
-	                        //Faire bouger la fusee a droite
-	                        printf("Mouvement vers la droite\n");
-	                        continue;
-	                    case SDLK_LEFT:
-	                        //Faire bouger la fusee a gauche
-	                        printf("Mouvement vers la gauche\n");
-	                        continue;
-	                    case SDLK_UP:
-	                        //Faire bouger la fusee vers le haut
-	                        printf("Mouvement vers le haut\n");
-	                        continue;
-	                    case SDLK_DOWN:
-	                        //Faire bouger la fusee vers le bas
-	                        printf("Mouvement vers le bas\n");
-	                        continue;
-	                    default:
-	                        continue;
-	                }
-	            
-	            //Evenemnts souris  	                
-	            //Touches souris
-	            case SDL_MOUSEBUTTONDOWN:
-	                // Recuperer position du clic si clic droit
-	                int x = event.button.x;
-	                int y = event.button.y;
+    /*----------------------------------------------------------------------------------*/
 
-	                //Verifier si le clic est dans le rectangle start
-	                if(x >= (LARGEUR_FENETRE/2 - LARGEUR_BOUTON/2) && x<= (LARGEUR_FENETRE/2 + LARGEUR_BOUTON/2) && y >= (HAUTEUR_FENETRE/3 - HAUTEUR_BOUTON/2) && y <= (HAUTEUR_FENETRE/3 + HAUTEUR_BOUTON/2))
-	                {
-	                    printf("Clic sur bouton start en %d / %d\n", x, y);
-	                    creationFusee(window,renderer, &texture_bouton_start, &texture_bouton_exit);
-	                    
-	                }
-	                
-	                //Verifier si le clic est dans le rectangle exit
-	                if(x >= (LARGEUR_FENETRE/2 - LARGEUR_BOUTON/2) && x<= (LARGEUR_FENETRE/2 + LARGEUR_BOUTON/2) && y >= (2*HAUTEUR_FENETRE/3 - HAUTEUR_BOUTON/2) && y <= (2*HAUTEUR_FENETRE/3 + HAUTEUR_BOUTON/2))
-	                {
-	                    printf("Clic sur bouton exit en %d / %d\n", x, y);
-	                    program_launched = SDL_FALSE;
-	                }
-	                continue;
-	                
-	            case SDL_QUIT:
-	                program_launched = SDL_FALSE;
-	                break;
-	            
-	            default:
-	              break;
-	        }
-	    }
-	 }
-	/*----------------------------------------------------------------------------------*/
-	
-	//Nettoyage/Libeerer memoire
-	SDL_DestroyTexture(texture_arriere_plan);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-	
-	return EXIT_SUCCESS;
+    SDL_DestroyTexture(texture_arriere_plan);
+    SDL_DestroyTexture(texture_bouton_start);
+    SDL_DestroyTexture(texture_bouton_exit);
+    SDL_DestroyTexture(texture_fusee);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
+
